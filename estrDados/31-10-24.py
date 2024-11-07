0a84b0fa090019867e2222a06277f2cffcc35cae
leoeliel <leoeliel13@gmail.com>
2024-11-07 17:18:59 -0400

    final da atividade 31-10-24.py

--- estrDados/31-10-24.py
+++ estrDados/31-10-24.py
@@ -211,7 +211,7 @@ if __name__ == '__main__':
 
 #3
 
-import os
+'''import os
 
 def createNode(value):
     return {'value': value, 'next': None}
@@ -302,7 +302,7 @@ def updateNode(head):
 def deleteNode(head):
     
     while True:
-        to_be_found = int(input('\nDigite o valor a ser alterado ou digite -1 para sair\n'))
+        to_be_found = int(input('\nDigite o valor a ser DELETADO ou digite -1 para sair\n'))
         
         if to_be_found == -1:
             print('\nOperação encerrada\n')
@@ -337,16 +337,15 @@ def deleteNode(head):
                     prev_node['next'] = this_node['next']
 
                 print('\nNó excluido\n')
-                print('\nOs elementos da lista atualizada são: \n')
+                print(f'\nOs elementos da lista atualizada são: \n')
+                return head
 
-                showList(head)
             else:
                 print('\nExclusão cancelada\n')
         else:
-                print('\Valor não encontrado...\n')
-        
+            print('\Valor não encontrado...\n')
 
-
+    return head        
 
 def main():
     
@@ -355,9 +354,11 @@ def main():
     head = None
     tail = None
 
-    showList(head)
 
     while True:
+        
+        showList(head)
+        
         opt = int(input('\nDigite um valor inteiro\n-1 Para inserir no meio\n-2 Para inserir no final\n-3 Para alterar um  valor\n-4 Para deletar um  valor\n0 Para sair\n'))
         if opt == 0:
             break
@@ -371,18 +372,208 @@ def main():
             head, tail = InsertEnd(head, tail, value)
         
         elif opt == -3:
-            updateNode(head)
+            head = updateNode(head)
 
         elif opt == -4:
-            deleteNode(head)
+            head = deleteNode(head)
 
         else:
             value = int(input('Digite o valor a ser inserido no inicio: '))
             head, tail = insertBegining(head, tail, value)
         
-        showList(head)
-
     os.system('clear')
+
+if __name__ == '__main__':
+    main()'''
+
+#4
+
+import os
+
+def createNode(value):
+    return {'value':value, 'next': None, 'prev': None} 
+
+def showList(head):
+    if head is None:
+        print('\nA lista está vazia\n')
+    else:
+        this_node = head
+        elems = []
+        while this_node is not None:
+            elems.append(str(this_node['value']))
+            this_node = this_node['next']
+        print(f'\nA lista é: {" -> ".join(elems)}\n')
+    
+def insertBegining(head, tail, value):
+    new_node = createNode(value)
+    if head is None:
+        tail = new_node
+    new_node['next'] = head
+    return new_node, tail
+
+def insertMiddle(head, tail, value, pos):
+    new_node = createNode(value)
+    if head is None:
+        return new_node, new_node
+    if pos == 1:
+        new_node['next'] = head
+        return new_node, tail
+    this_node = head
+    counter = 1
+    while counter < pos - 1 and this_node is not None:
+        this_node = this_node['next']
+        counter += 1
+    if this_node is None:
+        return insertEnding(head, tail, value)
+
+    new_node['next'] = this_node['next']
+    this_node['next'] = new_node
+    return head, tail
+
+def insertEnding(head, tail, value):
+    new_node = createNode(value)
+    if tail is not None:
+        tail['next'] = new_node
+    else:
+        head = new_node
+    return head, tail
+
+def updateNode(head, tail):
+    while True:
+        to_be_found = int(input('\nDigite o valor que você deseja alterar ou tecle -1 para sair:\n'))
+
+        if to_be_found == -1:
+            print('\nOperação encerrada...\n')
+            break
+
+        this_node = head
+        pos = 1
+        found = False
+
+        while this_node is not None:
+            if this_node['value'] == to_be_found:
+                
+                found = True
+                break
+        
+            this_node = this_node['next']
+            pos += 1
+        
+        if not found:
+            this_node = tail
+            pos = -1
+            while this_node is not None:
+                if this_node['value'] == to_be_found:
+                
+                    found = True
+                    break
+        
+                this_node = this_node['prev']
+                pos -= 1            
+
+        if found:
+            print(f'\nO valor {to_be_found} foi encontrado na posicao: {pos}\n')
+            new_value = input(f'\nDigite um novo valor para este nó: ')
+
+            this_node['value'] = new_value
+
+            print(f'\nO valor do nó na posicao {pos} foi alterado para: {new_value}\n')
+        else:
+            print('\nValor nao encontrado...\n')
+        
+        return head, tail
+
+def deleteNode(head, tail):
+    this_node = head
+    prev_node = None
+    pos = 1
+    found = False
+    
+    while True:
+        
+        to_be_found = int(input('\nDigite o valor que voce deseja excluir ou tecle -1 para sair: \n'))
+
+        if to_be_found == -1:
+            print('Operacao encerrada...\n')
+            break
+
+
+        while this_node is not None:
+            if this_node['value'] == to_be_found:
+                found = True
+                break
+            prev_node = this_node
+            this_node = this_node['next']
+            pos += 1
+
+        if not found:
+            this_node = tail
+            prev_node = None
+            pos = -1
+            while this_node is not None:
+                if this_node['value'] == to_be_found:
+                
+                    found = True
+                    break
+                
+                prev_node = this_node
+                this_node = this_node['prev']
+                pos -= 1
+
+
+        if found:
+            print(f'O valor {to_be_found} foi encontrado na posicao: {pos}')
+
+            yes_or_no = input(f'\nDeseja realmente exclui-lo? (sim/nao): \n')
+
+            if yes_or_no.lower() == 'sim':
+                if prev_node is None:
+                    head = this_node['next']
+                else:
+                    prev_node['next'] = this_node['next']
+            
+                print(f'\nO valor na posicao {pos} foi excluido\n')
+            else:
+                print('\nExclusao cancelada.\n')    
+        else:
+            print('\nValor nao encontrado...\n')
+        return head, tail
+
+def main():
+
+    head = None
+    tail = None
+
+    while True:
+        
+        os.system('clear')
+        showList(head)
+        
+        opt = int(input('\nDigite um valor inteiro\n-1 Para inserir no meio\n-2 Para inserir no final\n-3 Para alterar um  valor\n-4 Para deletar um  valor\n0 Para sair\n'))
+ 
+        if opt == 0:
+            os.system('clear')
+            break
+ 
+        elif opt == -1:
+            value = int(input('Digite o valor a ser inserido no meio: '))
+            pos = int(input('Digite a posicao para inserir o valor: '))
+ 
+            head, tail = insertMiddle(head, tail, value, pos)
+        
+        elif opt == -2:
+            value = int(input('Digite o valor a ser inserido no final: '))
+            head, tail = insertEnding(head, tail, value)
+        
+        elif opt == -3:
+            head, tail = updateNode(head, tail)
+
+        elif opt == -4:
+            head, tail = deleteNode(head, tail)
+
+        else:
+            value = int(input('Digite o valor a ser inserido no inicio: '))
+            head, tail = insertBegining(head, tail, value)
 
 if __name__ == '__main__':
     main()
